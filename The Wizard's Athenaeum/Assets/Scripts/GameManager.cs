1b684442637fe9317464c44384522866216f472b
Hanna Dodd <hrdodd@gmail.com>
2022-02-28 22:49:43 -0500

    Finishing Roguelike tutorial

---
+++ The Wizard's Athenaeum/Assets/Scripts/GameManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public float levelStartDelay = 2f;
+    public float turnDelay = 0.1f;
+    public static GameManager instance = null;
+    public int playerHP = 100;
+    [HideInInspector] public bool playersTurn = true;
+
+    private BoardManager boardScript;
+    private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+
+    void Awake() {
+
+        if (instance == null) {
+
+            instance = this;
+
+        } else if (instance != this) {
+
+            Destroy(gameObject);
+
+        }
+
+        DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
+
+        InitGame();
+        
+    }
+
+    void OnLevelWasLoaded(int index) {
+
+        level++;
+        InitGame();
+
+    }
+
+    void InitGame() {
+
+        enemies.Clear();
+        boardScript.SetupScene(level);
+
+    }
+
+    public void GameOver() {
+
+        enabled = false;
+
+    }
+
+    void Update() {
+
+        if (playersTurn || enemiesMoving) {
+
+            return;
+
+        }
+
+        StartCoroutine(MoveEnemies());
+
+    }
+
+    public void AddEnemyToList(Enemy script) {
+
+        enemies.Add(script);
+
+    }
+
+    IEnumerator MoveEnemies() {
+
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+
+        if (enemies.Count == 0) {
+
+            yield return new WaitForSeconds(turnDelay);
+
+        }
+
+        for (int i = 0; i < enemies.Count; i++) {
+
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+
+    }
+    
+}
---
+++ The Wizard's Athenaeum/Assets/Scripts/Player.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MovingObject {
+
+    public int enemyDamage = 1;
+    public float restartLevelDelay = 1f;
+
+    private Animator animator;
+    private int hp;
+
+    protected override void Start() {
+
+        animator = GetComponent<Animator>();
+        hp = GameManager.instance.playerHP;
+
+        base.Start();
+        
+    }
+
+    private void OnDisable() {
+
+        GameManager.instance.playerHP = hp;
+
+    }
+
+    void Update() {
+
+        if (!GameManager.instance.playersTurn) {
+
+            return;
+
+        }
+
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int) Input.GetAxisRaw("Horizontal");
+        vertical = (int) Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0) {
+
+            vertical = 0;
+
+        }
+
+        if (horizontal != 0 || vertical != 0) {
+
+            AttemptMove<Wall>(horizontal, vertical);
+
+        }
+        
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir) {
+
+        base.AttemptMove<T>(xDir, yDir);
+        //RaycastHit2D hit;
+
+        CheckIfGameOver();
+
+        GameManager.instance.playersTurn = false;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.tag == "Exit") {
+
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+
+        } else if (other.tag == "Scroll") {
+
+            // add spell to player
+            other.gameObject.SetActive(false);
+
+        }
+
+    }
+
+    protected override void OnCantMove<T>(T component) {
+
+        //Wall hitWall = component as Wall;
+        // wall damaging, idk what to do here instead
+
+    }
+
+    private void Restart() {
+
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void LoseHP(int loss) {
+
+        //set hit animation
+        hp -= loss;
+        CheckIfGameOver();
+
+    }
+
+    private void CheckIfGameOver(){
+
+        if (hp <= 0) {
+
+            GameManager.instance.GameOver();
+
+        }
+
+    }
+
+}
